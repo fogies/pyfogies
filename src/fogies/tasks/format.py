d50a9c52eb7584a7df8f08831ba7a4b3bfe50ae2
James Fogarty <jayfo@users.noreply.github.com>
2025-12-19 10:25:54 -0800

    Start to Include Tasks

---
+++ src/fogies/tasks/format.py
@@ -0,0 +1,35 @@
+"""
+Task for applying code formatting.
+"""
+
+from invoke import Collection, task
+
+
+class FormatTasks:
+    """
+    Tasks for applying code formatting.
+    """
+
+    @task(name="format")
+    def task_format(context):
+        """
+        Apply code formatting.
+        """
+
+        context.run(
+            command=" ".join(
+                [
+                    "black",
+                    ".",
+                ]
+            ),
+        )
+
+    def get_collection(self) -> Collection:
+        """
+        Get a collection of tasks.
+        """
+        namespace = Collection("format")
+        namespace.add_task(self.task_format)
+        
+        return namespace
