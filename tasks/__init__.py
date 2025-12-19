dad933be106fb33f7d04fe0ee6217c720dc7269a
James Fogarty <jayfo@users.noreply.github.com>
2025-12-19 10:25:54 -0800

    Start to Include Tasks

---
+++ tasks/__init__.py
@@ -0,0 +1,15 @@
+"""Invoke tasks for this project."""
+
+import colorama
+from invoke import Collection
+
+import fogies.tasks.format
+
+# Root namespace for tasks.
+namespace: Collection = Collection()
+
+# Enable color output.
+colorama.init()
+
+# TODO: This still feels messy, will need a bettery way to compose tasks as more develop.
+namespace.add_task(fogies.tasks.format.FormatTasks().get_collection()["format"])
