00a1fba386c8d1464e3a247e3b71f71e46816319
James Fogarty <jayfo@users.noreply.github.com>
2026-02-08 14:06:52 -0800

    Improve Naming

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -11,7 +11,7 @@ import fogies.tasks.poetry
 import fogies.tasks.test
 
 # Path to the Poetry secrets configuration file.
-PATH_SECRETS_POETRY = Path("secrets", "poetry.toml")
+_PATH_SECRETS_POETRY = Path("secrets", "poetry.toml")
 
 # Root namespace for tasks.
 namespace: Collection = Collection()
@@ -24,5 +24,5 @@ namespace.add_task(fogies.tasks.format.get_task_format())
 namespace.add_task(fogies.tasks.lint.get_task_lint())
 namespace.add_task(fogies.tasks.test.get_task_test())
 namespace.add_collection(
-    fogies.tasks.poetry.get_collection(path_secrets_poetry=PATH_SECRETS_POETRY)
+    fogies.tasks.poetry.get_collection(path_secrets_poetry=_PATH_SECRETS_POETRY)
 )
