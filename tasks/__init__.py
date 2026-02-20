2e9b4edc40b6c8b2e89e984b64e249b328aca60a
James Fogarty <jayfo@users.noreply.github.com>
2026-02-20 05:31:53 -0800

    Tasks for Subsets of Tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -19,10 +19,24 @@ namespace: Collection = Collection()
 # Enable color output.
 colorama.init()
 
-# Add tasks to the root namespace.
+# Tasks in the root collection.
 namespace.add_task(fogies.tasks.format.get_task_format())
 namespace.add_task(fogies.tasks.lint.get_task_lint())
-namespace.add_task(fogies.tasks.test.get_task_test())
 namespace.add_collection(
     fogies.tasks.poetry.get_collection(path_secrets_poetry=_PATH_SECRETS_POETRY)
 )
+
+# A collection for subsets of tests.
+_task_all = fogies.tasks.test.get_task_test()
+_task_integration = fogies.tasks.test.get_task_test("tests/integration")
+_task_terraform = fogies.tasks.test.get_task_test("tests/terraform")
+_task_tools = fogies.tasks.test.get_task_test("tests/tools")
+_task_unit = fogies.tasks.test.get_task_test("tests/unit")
+
+_collection_tests = Collection("test")
+_collection_tests.add_task(_task_all, name="all")
+_collection_tests.add_task(_task_integration, name="integration")
+_collection_tests.add_task(_task_terraform, name="terraform")
+_collection_tests.add_task(_task_tools, name="tools")
+_collection_tests.add_task(_task_unit, name="unit")
+namespace.add_collection(_collection_tests)
