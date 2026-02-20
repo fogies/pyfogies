6478aa142ec1ce688db5c54e0627fe70206c4c8e
James Fogarty <jayfo@users.noreply.github.com>
2026-02-20 03:47:30 -0800

    Beginning to Develop Terraform Backend

---
+++ terraform/backend/variables.tf
@@ -0,0 +1,23 @@
+variable "name_prefix" {
+  description = "Base prefix for backend resources (used to derive bucket and lock names)."
+  type        = string
+}
+
+variable "states" {
+  description = "Logical names of Terraform states to manage within this backend."
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Tags to apply to backend resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "force_destroy" {
+  description = "Whether to allow force destruction of the S3 bucket."
+  type        = bool
+  default     = false
+}
+
