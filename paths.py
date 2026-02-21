aa9bf6600aab9ee9889a2e6924a7f2e4744d442f
James Fogarty <jayfo@users.noreply.github.com>
2026-02-20 16:58:31 -0800

    Basic Test of Toolking

---
+++ paths.py
@@ -0,0 +1,9 @@
+"""Paths used by tasks and tests in this development environment."""
+
+from pathlib import Path
+
+# Binary cache directory.
+PATH_BINARY_CACHE = Path(".bin")
+
+# Path to the Poetry secrets configuration file.
+PATH_SECRETS_POETRY = Path("secrets", "poetry.toml")
