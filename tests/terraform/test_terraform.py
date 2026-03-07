1d1ae76b66708e86173fc83e56b4a0be9751a3fb
James Fogarty <jayfo@users.noreply.github.com>
2026-03-07 15:57:38 -0800

    Separate Tool Test

--- tests/terraform/test_terraform.py
+++ tests/terraform/test_terraform.py
@@ -1,29 +1,12 @@
 """Test Terraform tool."""
 
 import pathlib
-import subprocess
 
 from paths import PATH_STAGING_BINARY_CACHE
 from pydantic import BaseModel
 
 from fogies.tools.command import CommandParams
 from fogies.tools.terraform import terraform, terraform_tfvars
-
-
-def test_terraform_is_available() -> None:
-    """Context manager provides a working executable."""
-    with terraform(binary_cache_path=PATH_STAGING_BINARY_CACHE) as tf:
-        # Verify the executable exists.
-        assert tf.path.exists()
-
-        # Run the executable and verify the version matches.
-        result = subprocess.run(
-            [str(tf.path), "--version"],
-            capture_output=True,
-            text=True,
-        )
-        assert result.returncode == 0
-        assert tf.version in result.stdout
 
 
 def test_terraform_tfvars(tmp_path: pathlib.Path) -> None:
