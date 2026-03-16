01df7cf09118d879722562779214eeaf74234b7f
James Fogarty <jayfo@users.noreply.github.com>
2026-03-15 19:20:11 -0700

    Rename

--- tests/terraform/confbackend.py
+++
@@ -1,8 +0,0 @@
-"""Tests for the Terraform backend module."""
-
-# Name of backend to be shared across all Terraform testing.
-PYFOGIES_TEST_TERRAFORM_BACKEND_NAME: str = "pyfogies-test-backend"
-PYFOGIES_TEST_TERRAFORM_BACKEND_REGION: str = "us-west-2"
-PYFOGIES_TEST_TERRAFORM_BACKEND_STATES: list[str] = [
-    "test-backend",
-]
