34e29d63ab4172f0548b5b096cec98564a037ab7
James Fogarty <jayfo@users.noreply.github.com>
2026-06-30 08:14:21 -0700

    Additional Backend

--- tests/terraform/backend.py
+++ tests/terraform/backend.py
@@ -8,4 +8,9 @@ PYFOGIES_TEST_TERRAFORM_BACKEND_REGION: str = "us-west-2"
 
 
 class PYFOGIES_TEST_TERRAFORM_BACKEND_STATES(str, Enum):
+    PYFOGIES_TEST_CERTIFICATE = "pyfogies-test-certificate"
+    TEST_ALB = "test-alb"
     TEST_BACKEND = "test-backend"
+    TEST_CERTIFICATE = "test-certificate"
+    TEST_ECR = "test-ecr"
+    TEST_NETWORK = "test-network"
