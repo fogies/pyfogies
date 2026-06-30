d79665e82055c65d706acb02d2b8a9ec877abc76
James Fogarty <jayfo@users.noreply.github.com>
2026-06-30 08:15:14 -0700

    Fixture Discovery

--- tests/terraform/conftest.py
+++ tests/terraform/conftest.py
@@ -3,6 +3,10 @@
 from tests.terraform.pyfogies_test_backend import (
     pyfogies_test_backend as _pyfogies_test_backend,
 )
+from tests.terraform.pyfogies_test_certificate import (
+    pyfogies_test_certificate as _pyfogies_test_certificate,
+)
 
 # Exported for pytest discovery.
 pyfogies_test_backend = _pyfogies_test_backend
+pyfogies_test_certificate = _pyfogies_test_certificate
