969a9274a3fb62b47399531ba6491940608319c2
James Fogarty <jayfo@users.noreply.github.com>
2026-07-23 21:13:44 -0700

    Add Flag for Creation of Hosted Zones

--- terraform/hosted_zone/hosted_zone.tf
+++ terraform/hosted_zone/hosted_zone.tf
@@ -1,15 +1,27 @@
+data "aws_route53_zone" "existing" {
+  count = var.create_zone ? 0 : 1
+  name  = var.zone_name
+}
+
 resource "aws_route53_zone" "zone" {
-  name = var.zone_name
+  count = var.create_zone ? 1 : 0
+  name  = var.zone_name
 }
 
 resource "aws_route53domains_registered_domain" "domain" {
+  count       = var.create_zone ? 1 : 0
   provider    = aws.us_east_1
   domain_name = var.zone_name
 
   dynamic "name_server" {
-    for_each = toset(aws_route53_zone.zone.name_servers)
+    for_each = toset(aws_route53_zone.zone[0].name_servers)
     content {
       name = name_server.value
     }
   }
+}
+
+locals {
+  zone_id      = var.create_zone ? aws_route53_zone.zone[0].zone_id : data.aws_route53_zone.existing[0].zone_id
+  name_servers = var.create_zone ? aws_route53_zone.zone[0].name_servers : data.aws_route53_zone.existing[0].name_servers
 }
--- terraform/hosted_zone/outputs.tf
+++ terraform/hosted_zone/outputs.tf
@@ -1,14 +1,14 @@
 output "zone_id" {
   description = "Route 53 hosted zone ID."
-  value       = aws_route53_zone.zone.zone_id
+  value       = local.zone_id
 }
 
 output "zone_name" {
   description = "Hosted zone domain name."
-  value       = aws_route53_zone.zone.name
+  value       = var.zone_name
 }
 
 output "name_servers" {
   description = "Name servers assigned to the hosted zone."
-  value       = aws_route53_zone.zone.name_servers
+  value       = local.name_servers
 }
